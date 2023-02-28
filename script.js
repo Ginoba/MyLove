8b4e32c12a73a8802aa6e73352f7c8408007b5b8
Gino <ginococeres2000@gmail.com>
2023-02-28 17:32:55 -0300

    Agregado un JS para el link

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".primary-btn");
+
+btn.addEventListener("click", redireccion);
+
+function redireccion () {
+  location.href = "https://ginoba.github.io/MyLove2/"
+}
